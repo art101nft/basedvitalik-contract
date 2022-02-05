68dc82fb1427df221e7ecba4fa27f2b200703e21
lza_menace <lza_menace@protonmail.com>
2022-02-05 02:13:59 -0800

    add simple script ot update price

---
+++ scripts/setSalePrice.js
@@ -0,0 +1,19 @@
+const newPrice = '10000000000000000'; // .01
+
+module.exports = async function main(callback) {
+  try {
+    const BasedVitalik = artifacts.require("BasedVitalik");
+    const contract = await BasedVitalik.deployed();
+    if (newPrice == '') {
+      console.log('You need to specify a new sale price.');
+      callback(1);
+    } else {
+      await contract.setSalePrice(newPrice);
+      console.log(`Set new sale price to ${newPrice}`);
+      callback(0);
+    }
+  } catch (error) {
+    console.error(error);
+    callback(1);
+  }
+}
