e6897a49c0929782e91edd703871bfbcfe6d7188
lza_menace <lza_menace@protonmail.com>
2022-02-13 00:22:00 -0800

    update passing in proxy addrs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,12 +2,12 @@ var BasedVitalik = artifacts.require("BasedVitalik");
 
 module.exports = function(deployer) {
   let addr;
-  if (deployer.network == 'testnet') {
-    console.log('[+] Using OpenSea testnet proxy address 0xf57b2c51ded3a29e6891aba85459d600256cf317');
-    addr = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
-  } else {
+  if (deployer.network == 'mainnet') {
     console.log('[+] Using OpenSea mainnet proxy address 0xa5409ec958c83c3f309868babaca7c86dcb077c1');
     addr = '0xa5409ec958c83c3f309868babaca7c86dcb077c1';
+  } else {
+    console.log('[+] Using OpenSea testnet proxy address 0xf57b2c51ded3a29e6891aba85459d600256cf317');
+    addr = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
   }
   deployer.deploy(BasedVitalik, addr);
 };
