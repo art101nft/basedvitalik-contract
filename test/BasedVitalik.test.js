101177d31193c029ab19ed78c53a9fe73094edef
lza_menace <lza_menace@protonmail.com>
2022-02-14 16:17:59 -0800

    updating tests with gas estimates

--- test/BasedVitalik.test.js
+++ test/BasedVitalik.test.js
@@ -136,6 +136,9 @@ contract('BasedVitalik', function ([owner, other, other2]) {
 
   it('reserve func works once and mints 40 to owner', async function () {
     await this.bv.reserveVitaliks();
+    const _gas = await this.bv.reserveVitaliks.estimateGas();
+    const _gasEth = web3.utils.fromWei(web3.utils.toWei(new BN(_gas.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show reserving 40 vitaliks will require ${_gas} gas. At 150 gwei this transaction would cost ${_gasEth * 150} ETH`);
     await expect(
       (await this.bv.totalSupply()).toString()
     ).to.equal('40');
@@ -150,9 +153,48 @@ contract('BasedVitalik', function ([owner, other, other2]) {
 
   it('early access mode w/ merkle root hash allows whitelist minting', async function () {
     const _val = new BN('30000000000000000');
+    const _val2 = new BN('60000000000000000');
+    const _val10 = new BN('300000000000000000');
+    const _val20 = new BN('600000000000000000');
     let root = proofs.root.Proof[0];
     await this.bv.setMerkleRoot(root);
     await this.bv.toggleMinting();
+    const _gas1 = await this.bv.mintVitaliks.estimateGas(
+      proofs[other].Index,
+      other,
+      proofs[other].Amount,
+      proofs[other].Proof,
+      1, {value: _val, from: other}
+    );
+    const _gasEth1 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas1.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 1 during early access mode (w/ merkle proof validation) will require ${_gas1} gas. At 150 gwei this transaction would cost ${_gasEth1 * 150} ETH`);
+    const _gas2 = await this.bv.mintVitaliks.estimateGas(
+      proofs[other].Index,
+      other,
+      proofs[other].Amount,
+      proofs[other].Proof,
+      2, {value: _val2, from: other}
+    );
+    const _gasEth2 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas2.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 2 during early access mode (w/ merkle proof validation) will require ${_gas2} gas. At 150 gwei this transaction would cost ${_gasEth2 * 150} ETH`);
+    const _gas10 = await this.bv.mintVitaliks.estimateGas(
+      proofs[other].Index,
+      other,
+      proofs[other].Amount,
+      proofs[other].Proof,
+      10, {value: _val10, from: other}
+    );
+    const _gasEth10 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas10.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 10 during early access mode (w/ merkle proof validation) will require ${_gas10} gas. At 150 gwei this transaction would cost ${_gasEth10 * 150} ETH`);
+    const _gas20 = await this.bv.mintVitaliks.estimateGas(
+      proofs[other].Index,
+      other,
+      proofs[other].Amount,
+      proofs[other].Proof,
+      20, {value: _val20, from: other}
+    );
+    const _gasEth20 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas20.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 20 during early access mode (w/ merkle proof validation) will require ${_gas20} gas. At 150 gwei this transaction would cost ${_gasEth20 * 150} ETH`);
     await this.bv.mintVitaliks(
       proofs[other].Index,
       other,
@@ -173,9 +215,22 @@ contract('BasedVitalik', function ([owner, other, other2]) {
     const _buy1 = new BN('30000000000000000');
     const _buy2 = new BN('60000000000000000');
     const _buy3 = new BN('90000000000000000');
-    const _buy10 = new BN('300000000000000000');
+    const _buy5 = new BN('150000000000000000');
+    const _buy6 = new BN('180000000000000000');
     await this.bv.toggleMinting();
     await this.bv.toggleEarlyAccessMode();
+    const _gas1 = await this.bv.mintVitaliks.estimateGas(0, other, 0, [], 1, {value: _buy1, from: other});
+    const _gas2 = await this.bv.mintVitaliks.estimateGas(0, other, 0, [], 2, {value: _buy2, from: other});
+    const _gas3 = await this.bv.mintVitaliks.estimateGas(0, other, 0, [], 3, {value: _buy3, from: other});
+    const _gas5 = await this.bv.mintVitaliks.estimateGas(0, other, 0, [], 5, {value: _buy5, from: other});
+    const _gasEth1 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas1.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 1 during public mint (w/o merkle proof validation) will require ${_gas1} gas. At 150 gwei this transaction would cost ${_gasEth1 * 150} ETH`);
+    const _gasEth2 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas2.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 2 during public mint (w/o merkle proof validation) will require ${_gas2} gas. At 150 gwei this transaction would cost ${_gasEth2 * 150} ETH`);
+    const _gasEth3 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas3.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 3 during public mint (w/o merkle proof validation) will require ${_gas3} gas. At 150 gwei this transaction would cost ${_gasEth3 * 150} ETH`);
+    const _gasEth5 = web3.utils.fromWei(web3.utils.toWei(new BN(_gas5.toString()), 'gwei'), 'ether');
+    console.log(`[+] Estimates show minting 5 during public mint (w/o merkle proof validation) will require ${_gas5} gas. At 150 gwei this transaction would cost ${_gasEth5 * 150} ETH`);
     await this.bv.mintVitaliks(0, other, 0, [], 1, {value: _buy1, from: other});
     await expect(
       (await this.bv.totalSupply()).toString()
@@ -185,11 +240,11 @@ contract('BasedVitalik', function ([owner, other, other2]) {
       (await this.bv.totalSupply()).toString()
     ).to.equal('3');
     await expectRevert(
-      this.bv.mintVitaliks(0, other, 0, [], 4, {value: _buy10, from: other}),
+      this.bv.mintVitaliks(0, other, 0, [], 4, {value: _buy5, from: other}),
       'Incorrect Ether supplied for the number of tokens requested.',
     );
     await expectRevert(
-      this.bv.mintVitaliks(0, other, 0, [], 10, {value: _buy10, from: other}),
+      this.bv.mintVitaliks(0, other, 0, [], 6, {value: _buy6, from: other}),
       'Cannot mint more than 5 per tx during public sale.',
     );
   });
