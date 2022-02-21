b5c7ddd36a1ca2647969e4ba9896037aa6ff5c57
lza_menace <lza_menace@protonmail.com>
2022-02-21 14:19:55 -0800

    add script to show contract details

---
+++ scripts/getInfo.js
@@ -0,0 +1,36 @@
+module.exports = async function main(callback) {
+  try {
+    const BasedVitalik = artifacts.require("BasedVitalik");
+    const contract = await BasedVitalik.deployed();
+    await contract.withdraw();
+    const merkleSet = await contract.merkleSet();
+    const earlyAccessMode = await contract.earlyAccessMode();
+    const mintingIsActive = await contract.mintingIsActive();
+    const reservedVitaliks = await contract.reservedVitaliks();
+    const placeholderMeta = await contract.placeholderMeta();
+    const salePriceWei = await contract.salePrice();
+    const salePriceEth = web3.utils.fromWei(salePriceWei);
+    const maxSupply = await contract.maxSupply();
+    const maxMints = await contract.maxMints();
+    const baseURI = await contract.baseURI();
+    const tokenURI = await contract.tokenURI(1);
+    const _contractURI = await contract._contractURI();
+    console.log(`[+] Mint info:`)
+    console.log(`- merkleSet: ${merkleSet}`);
+    console.log(`- earlyAccessMode: ${earlyAccessMode}`);
+    console.log(`- mintingIsActive: ${mintingIsActive}`);
+    console.log(`- reservedVitaliks: ${reservedVitaliks}`);
+    console.log(`- placeholderMeta: ${placeholderMeta}`);
+    console.log(`- salePriceWei: ${salePriceWei}`);
+    console.log(`- salePriceEth: ${salePriceEth}`);
+    console.log(`- maxSupply: ${maxSupply}`);
+    console.log(`- maxMints: ${maxMints}`);
+    console.log(`- baseURI: ${baseURI}`);
+    console.log(`- tokenURI(1): ${tokenURI}`);
+    console.log(`- _contractURI: ${_contractURI}`);
+    callback(0);
+  } catch (error) {
+    console.error(error);
+    callback(1);
+  }
+}
