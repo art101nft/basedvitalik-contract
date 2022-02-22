ad53acf97636b32aac8f6fbcd795e7a640b2ac87
lza_menace <lza_menace@protonmail.com>
2022-02-22 11:52:37 -0800

    update to v2 whitelist

--- scripts/1_setMerkleRoot.js
+++ scripts/1_setMerkleRoot.js
@@ -1,4 +1,4 @@
-const newMerkleRoot = '0x6a85c81e2211c12bcd93c02f2f23029fa7ebce5331f4b8a1058a187ef95e6c28'; // mainnet, v1 WL feb22 12:30 AM
+const newMerkleRoot = '0x169752f4056168b6e3fe958637366b916de99a0c6aa0c44a81a2a8e61606abad'; // mainnet, v2 WL feb22 11:50 AM
 
 module.exports = async function main(callback) {
   try {
