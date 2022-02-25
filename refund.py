52fa166d3cf1e7b0aa7e0cf1a3ee070187bdaa7f
lza_menace <lza_menace@protonmail.com>
2022-02-25 14:49:48 -0800

    skipping some addresses

--- refund.py
+++ refund.py
@@ -41,10 +41,10 @@ with open('export-0xea2dc6f116a4c3d6a15f06b4e8ad582a07c3dd9c.csv', mode='r')as f
     csv_file = csv.DictReader(file)
     _m = '0xe366c9ca'
     for lines in csv_file:
-        if lines['Method'] == _m and lines['Status'] == '':
-            tx_hash = lines["Txhash"]
-            if tx_hash in open('sent.log').read():
-                continue
+        tx_hash = lines['Txhash']
+        refund_address = lines['From']
+        skip = ['0x482979F11f17C853f3A526A28d5D9381Cd8aa635']
+        if lines['Method'] == _m and lines['Status'] == '' and tx_hash not in open('sent.log').read() and refund_address not in skip:
             amt_eth = float(lines["Value_IN(ETH)"])
             amt_wei = w3.toWei(amt_eth, 'ether')
             acc = get_eth_account()
@@ -52,7 +52,7 @@ with open('export-0xea2dc6f116a4c3d6a15f06b4e8ad582a07c3dd9c.csv', mode='r')as f
             from_address = w3.toChecksumAddress(acc[0])
             eth_account = w3.eth.account.from_key(from_privkey)
             w3.defaultAccount = from_address
-            to_address = w3.toChecksumAddress(lines['From'])
+            to_address = w3.toChecksumAddress(refund_address)
             if to_address not in totals:
                 totals[to_address] = 0
             totals[to_address] += amt_wei
@@ -71,19 +71,20 @@ with open('export-0xea2dc6f116a4c3d6a15f06b4e8ad582a07c3dd9c.csv', mode='r')as f
                 'nonce': nonce,
                 'chainId': 1
             }
-            print(f'[+] Refunding {amt_eth} ETH to {shorten(to_address)} at {gas_price_gwei} gwei and nonce {nonce} for mint tx {shorten(tx_hash)}')
+            print(f'[+] Refunding {amt_eth} ETH to {shorten(to_address)} at {round(gas_price_gwei, 3)} gwei and nonce {nonce} for mint tx {shorten(tx_hash)}')
             # break
+            sleep(12)
             signed_tx = eth_account.sign_transaction(tx_dict)
             res = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
             refund_hash = res.hex()
             with open('sent.log', 'a') as f:
                 f.write(f'{tx_hash} {refund_hash}\n')
             try:
-                sleep(30)
+                sleep(24)
                 tx_receipt = w3.eth.wait_for_transaction_receipt(refund_hash, timeout=600, poll_latency=15)
                 tx_status = tx_receipt['status'] == 1
                 if not tx_status:
-                    print(f'[ethereum_tx] Tx {refund_hash} failed')
+                    print(f'[!] Tx {refund_hash} failed')
                     raise Exception('TX failed')
                 print(f'[+] Payout details updated for tx {eth_tx.tx_hash}')
             except TransactionNotFound:
